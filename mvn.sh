4361a8b6a1b3fd7ffd6bb287ed66dd48214ca006
Renato Ribeiro <renato.ribeiro@hotmart.com>
2019-10-09 22:55:12 -0300

    new feature

--- mvn.sh
+++ mvn.sh
@@ -4,9 +4,12 @@
 MESSAGE="$(git log --format=%B -n 1)"
 
 if [[ $MESSAGE =~ '[MAJOR]' ]]; then
-  echo "It's there!"
+  mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0
 elif [[ $MESSAGE =~ '[MINOR]' ]]; then
- echo "Its minor"
+  mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0
 else
-	echo "none"
+	  mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}
 fi
+
+git commmit -am "Incrementing pom.xml version"
+git push -u origin master
